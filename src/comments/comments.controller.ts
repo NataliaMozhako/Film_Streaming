3682fe722e61a38311f5f44776a5105ac8e9e05f
Elizaveta0 <51176445+Elizaveta0@users.noreply.github.com>
2021-12-07 12:17:50 +0300

    Comments guard

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards, UsePipes } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -19,6 +20,7 @@ export class CommentsController {
     return this.commentsService.getById(id)
   }
 
+  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   @Post()
   create(@Body() createCreateDto: CreateCommentDto): Promise<Comment> {
